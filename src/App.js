3e03a776b5032d6632598ab20efd9fc5e5a5ecfd
Renee Graham <renee.noluthando@gmail.com>
2021-02-16 15:42:28 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [inputNumber, setInputNumber] = useState(1);
+  const [answer, setAnswer] = useState("")
+  const [fizzCounter, setFizzCounter] = useState(0)
+  const [buzzCounter, setBuzzCounter] = useState(0)
+  const [fizzBuzzCounter, setFizzBuzzCounter] = useState(0)
+
+useEffect(() =>{
+  console.log("user effect triggered")
+  if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+    setAnswer("fizzbuzz")
+    setFizzBuzzCounter (fizzBuzzCounter + 1)
+  } else if (inputNumber % 3 === 0) {
+    setAnswer("fizz")
+    setFizzCounter (fizzCounter + 1)
+  } else if (inputNumber % 5 === 0) {
+    setAnswer("buzz")
+    setBuzzCounter (buzzCounter +1)
+  } else {
+    setAnswer(inputNumber);
+  }
+
+},[inputNumber])
+
+  const handleInc = () => {
+      setInputNumber(inputNumber + 1);
+  }
+
+  const handleGuess = (event) => {
+       event.preventDefault()
+       
+       console.log(event.target.value)
+  }
+
+  return ( 
+    <div>
+      <button value={inputNumber} onClick={handleInc}>+</button>
+      <form onSubmit={handleGuess}>
+      <input type="text" placeholder="enter your guess"/>
+      <input type="submit" value="submit guess"/>
+      </form>
+     <h2>fizzCounter is {fizzCounter}</h2>
+     <h2>buzzCounter is {buzzCounter}</h2>
+     <h2>fizzBuzzCounter is {fizzBuzzCounter}</h2>
+      <h2>Number is {inputNumber}</h2>
+      <h2>Answer is {answer}</h2>
     </div>
   );
 }
